e30bc0490d4471ff3305dcf63c3ebfd4a248327d
Darshan Chaudhary <darshan@zinnov.com>
2017-09-30 01:15:14 +0530

    add linked_list implementation

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
 
-int main(int argc, char *argv[])
+struct node{
+  int value;
+  struct node* next;
+};
+
+
+typedef struct {
+  struct node* root;
+} ll;
+
+void add_to_ll(int value, ll* linked_list) {
+  struct node new_node = {value, linked_list->root};
+  linked_list->root = &new_node;
+}
+
+void print_ll(ll* ll2) {
+  printf("%p", ll2);
+  struct node* temp = ll2->root;
+  while (temp->next != NULL) {
+    printf("%d ", temp->value);
+    temp = temp->next;
+  }
+}
+
+
+int main()
 {
-  puts("Hello World");
+  printf("Creating a linked list...\n");
+  struct node root_node = {1, NULL};
+  ll my_linked_list = { &root_node };
+  for (int i = 0; i < 10000; i++) {
+    add_to_ll(i, &my_linked_list);
+  }
+  printf("my_linked_list root value %d\n", my_linked_list.root->value);
   return 0;
 }
